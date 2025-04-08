ed132c560a2538d11d41fe105d2b58fad4a2cb13
Manisha Rana <manisharana02005@gmail.com>
2025-04-08 23:26:07 +0530

    added code for login using auth

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,6 +1,9 @@
 
 const bcrypt = require('bcrypt');
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { options } = require('../routes/user');
+require("dotenv").config();
 
 
 //signup route handler
@@ -46,5 +49,76 @@ exports.signup=async(req, res)=>{
             success:false,
             message:"user can not be registered"
         });
+    }
+}
+
+//login route handler
+exports.login = async(req, res)=>{
+    try{
+        //data fetch
+        const {email, password} = req.body;
+
+        //validation on email and password
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message: "Please fill all the details",
+            });
+        }
+
+        //check if the user is available
+        let user = await User.findOne({email});
+
+        //if not a registered user
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message: "user is not registered",
+            });
+        }
+
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role,
+        }
+        //varify password and generate a JWT token
+        if(await bcrypt.compare(password, user.password)){
+            //password matched
+            let token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:"2h"}); //generated a JWT token using sign method
+
+            user = user.toObject();
+            //token ko user me daal diya
+            user.token=token;
+            //removed password from user's object, not from database
+            user.password=undefined;
+
+            //create options
+            const options={
+                expires: new Date(Date.now()+3*24*60*60*1000),
+                httpOnly:true,
+            }
+            //create cookie
+            return res.cookie("manishaCookie", token, options).status(200).json({
+               success:true,
+               token,
+               user,
+               message:'user logged in successfully', 
+            });
+        }
+        else{
+            //password did not match
+            return res.status(403).json({
+                success:false,
+                message: "password incorrect",
+            });
+        }
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({
+            success:false,
+            message: "Login Failure",
+        });      
     }
 }
